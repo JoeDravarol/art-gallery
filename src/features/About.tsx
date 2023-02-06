8bffcd7ef3fc3f3cddf224b8cc4d23007fc20658
Joe Dravarol <joedravarol@gmail.com>
2023-02-06 19:18:03 +0000

    feat: Desktop About section

--- src/features/About.tsx
+++ src/features/About.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { Box, CardMedia, Grid, Stack, Typography } from '@mui/material';
 import { styled } from "@mui/material/styles";
 
+import imgDesktopRoomSm from 'assets/desktop/image-grid-1.jpg';
+import imgDesktopRoomMd from 'assets/desktop/image-grid-1@2x.jpg';
 import imgTabletRoomSm from 'assets/tablet/image-grid-1.jpg';
 import imgTabletRoomMd from 'assets/tablet/image-grid-1@2x.jpg';
 import imgMobileRoomSm from 'assets/mobile/image-grid-1.jpg';
 import imgMobileRoomMd from 'assets/mobile/image-grid-1@2x.jpg';
 
+import imgDesktopBenchSm from 'assets/desktop/image-grid-2.jpg';
+import imgDesktopBenchMd from 'assets/desktop/image-grid-2@2x.jpg';
 import imgTabletBenchSm from 'assets/tablet/image-grid-2.jpg';
 import imgTabletBenchMd from 'assets/tablet/image-grid-2@2x.jpg';
 import imgMobileBenchSm from 'assets/mobile/image-grid-2.jpg';
 import imgMobileBenchMd from 'assets/mobile/image-grid-2@2x.jpg';
 
+import imgDesktopGalleryMd from 'assets/desktop/image-grid-3@2x.jpg';
 import imgTabletGallerySm from 'assets/tablet/image-grid-3.jpg';
 import imgTabletGalleryMd from 'assets/tablet/image-grid-3@2x.jpg';
 import imgMobileGallerySm from 'assets/mobile/image-grid-3.jpg';
@@ -44,34 +49,53 @@ const StyledGrid = styled(Grid)(({ theme }) => ({
     'img': {
       height: '100%',
     }
+  },
+  [theme.breakpoints.up('desktop')]: {
+    gridTemplateColumns: '1fr 160px 445px',
+    gridTemplateRows: '400px 313px 377px',
+    gridGap: 30,
+    maxWidth: 1110,
+    marginInline: 'auto',
   }
 })) as typeof Grid;
 
 const About: React.FC = () => {
   return (
-    <StyledGrid component="main" container px={{ mobile: 2, tablet: 5 }} mb={15}>
+    <StyledGrid
+      component="main"
+      container
+      px={{ mobile: 2, tablet: 5, desktop: 'initial' }}
+      mb={{ mobile: 15, desktop: 22.5 }}
+    >
       <picture className="gallery-room">
+        <source srcSet={imgDesktopRoomMd} media="(min-width: 1600px)" />
+        <source srcSet={imgDesktopRoomSm} media="(min-width: 1440px)" />
         <source srcSet={imgTabletRoomMd} media="(min-width: 900px)" />
         <source srcSet={imgTabletRoomSm} media="(min-width: 768px)" />
         <source srcSet={imgMobileRoomMd} media="(min-width: 450px)" />
         <CardMedia component="img" src={imgMobileRoomSm} alt="Gallery room" />
       </picture>
 
-      <Box className="gallery-info" mb={2}>
+      <Box className="gallery-info" mb={2} maxWidth={{ desktop: 350 }} alignSelf="center">
         <Heading
-          sx={{ mt: '9px', mb: '21px' }}
           variant="h3"
           component="h2"
+          mt={{ mobile: '9px', desktop: 'initial' }}
+          mb={{ mobile: '21px', desktop: 4 }}
+          fontSize={{ desktop: '3.75rem' }}
+          lineHeight={{ desktop: '3.75rem' }}
         >
-          Your day <Box component="span" display="block">at the gallery</Box>
+          Your day <Box component="span" display={{ mobile: 'block', desktop: 'inline' }}>at the gallery</Box>
         </Heading>
 
-        <Typography variant="body2">
+        <Typography variant="body2" fontSize={{ desktop: '1.375rem' }} lineHeight={{ desktop: '2rem' }}>
           Wander through our distinct collections and find new insights about our artists. Dive into the details of their creative process.
         </Typography>
       </Box>
 
       <picture className="grid-item--large">
+        <source srcSet={imgDesktopBenchMd} media="(min-width: 1600px)" />
+        <source srcSet={imgDesktopBenchSm} media="(min-width: 1440px)" />
         <source srcSet={imgTabletBenchMd} media="(min-width: 900px)" />
         <source srcSet={imgTabletBenchSm} media="(min-width: 768px)" />
         <source srcSet={imgMobileBenchMd} media="(min-width: 450px)" />
@@ -79,6 +103,7 @@ const About: React.FC = () => {
       </picture>
 
       <picture>
+        <source srcSet={imgDesktopGalleryMd} media="(min-width: 1440px)" />
         <source srcSet={imgTabletGalleryMd} media="(min-width: 900px)" />
         <source srcSet={imgTabletGallerySm} media="(min-width: 768px)" />
         <source srcSet={imgMobileGalleryMd} media="(min-width: 450px)" />
@@ -87,8 +112,8 @@ const About: React.FC = () => {
 
       <Stack
         className="inspired-info"
-        px={{ mobile: 3, tablet: 3.5 }}
-        py={6}
+        px={{ mobile: 3, tablet: 3.5, desktop: 6 }}
+        py={{ mobile: 6, desktop: 8 }}
         bgcolor="secondary.main"
         color="common.white"
         justifyContent={{ tablet: 'center' }}
@@ -97,11 +122,13 @@ const About: React.FC = () => {
           variant="h3"
           component="h2"
           mb={3}
+          fontSize={{ desktop: '3.75rem' }}
+          lineHeight={{ desktop: '3.75rem' }}
         >
           Come & be inspired
         </Heading>
 
-        <Typography variant="body2">
+        <Typography variant="body2" fontSize={{ desktop: '1.375rem' }} lineHeight={{ desktop: '2rem' }}>
           We're excited to welcome you to our gallery and see how our collections influence you.
         </Typography>
       </Stack>
